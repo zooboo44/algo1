99a02441200abe09ff5a8403b5c3b734c663d3bc
Christian Ziobro <47931935+zooboo44@users.noreply.github.com>
2025-03-09 20:51:27 -0700

    Update algo.cpp

--- algo.cpp
+++ algo.cpp
@@ -1,82 +1,100 @@
-// Christian Ziobro
+// Christian Ziobro, ziobro44@csu.fullerton.edu
 #include <iostream>
 #include <vector>
 using namespace std;
 
-bool bfsCycle(int n, vector<vector<int>>& adj, 
-    vector<bool>& visited, int parent) {
-                      
-    visited[n] = true;
+bool bfsLoop(int i, int parent, vector<vector<int>>& adj, vector<bool>& visited) {
 
-    for(int i = 0; i < adj[n].size(); i++){
-        
-        if (!visited[i]) {
-            if (bfsCycle(i, adj, visited, n)){
+    //mark current node as visited in the visited array
+    visited[i] = true;
+
+    for (int child : adj[i]) {
+        //if the current node is not marked as visited, 
+        if (!visited[child]) {
+            //call bfsLoop recursively on the child node
+            if (bfsLoop(child, i, adj, visited)) {
                 return true;
             }
         }
         //base case for recursive function, if the current node does != the parent, return true as it is a cycle
-        else if (i != parent){
+        else if (child != parent) {
             return true;
         }
     }
-    
+
     return false;
 }
 
-bool isCycle(vector<vector<int>>& adj) {
-    //number of nodes(size of adjacent list vector)
+bool loopDetection(vector<vector<int>>& adj) {
+    //number of nodes (size of adjacent list vector)
     int n = adj.size();
-    
+
     //create vector of visited nodes, initialize all to false
     vector<bool> visited(n, false);
 
-    //iterate through adjacent list vector
+    //iterate through parent nodes, if the parent node hasn't been visited, it calls the bfsLoop function to determine if there is a loop
     for (int i = 0; i < n; i++) {
-        //if not visited (it wont be as this is the first call of the function)
-        if (!visited[i]){
-            //if the call to the bfsCycle function returns true, this function returns true as it is a cycle
-            if (bfsCycle(i, adj, visited, -1)){
+
+        if (!visited[i]) {
+            if (bfsLoop(i, -1, adj, visited)) { //calls bfsLoop on the 1st (0) node, which is why the parent variable is -1
                 return true;
             }
         }
     }
-    
-    //returns false as the graph is not a cycle
+
     return false;
+}
+
+void fillAdjacencyList(vector<vector<int>>& adj, int n) {
+    //iterate through vector of parent nodes
+    for (int parent = 0; parent < n; parent++) {
+        //prompts user to enter child node of current parent
+        cout << "Enter child nodes of parent: " << parent << ": " << endl;
+        int child = 0;
+        while (true) {
+            //takes user input of child nodes, if input == -1, then it moves on to the next parent
+            cin >> child;
+            if (child == -1) {
+                break;
+            }
+            //fills the adjacency list of the parent to the child, and the child to the parent to make an undirected graph
+            adj[parent].push_back(child);
+            adj[child].push_back(parent);
+        }
+    }
 }
 
 int main() {
     int n = 0;
     cout << "Enter the number of nodes: ";
     cin >> n;
-    vector<vector<int>> adj(n);
-
-    for(int parent = 0; parent < n; parent++){
-        cout << "Enter child nodes of parent: " << parent << ": " << endl;
-        int child = 0;
-        while(child != -1){
-            cin >> child;
-            adj[parent].push_back(child);
-            adj[child].push_back(parent);
-        }
+    if (n <= 0) {
+        cout << "Enter a whole number greater than 0" << endl;
+        exit(1);
     }
-
+    vector<vector<int>> adj(n);
+    //prompts user to fill out adjacency list
+    fillAdjacencyList(adj, n);
+    
+    //n = 3, loop detected
+    /*adj[1].push_back(0);
     adj[0].push_back(1);
+    adj[0].push_back(2);
+    adj[2].push_back(0);
+    adj[1].push_back(2);
+    adj[2].push_back(1);*/
+
+    //n = 3, no loop detected
+    /*adj[0].push_back(1);
     adj[1].push_back(0);
     adj[1].push_back(2);
-    adj[2].push_back(1);
-    adj[2].push_back(3);
-    adj[3].push_back(2);
-    adj[3].push_back(4);
-    adj[4].push_back(3);
-    adj[4].push_back(5);
-    adj[5].push_back(4);
-    
+    adj[2].push_back(1);*/
+
     //calls the isCycle function to determine if the graph is a cycle or not
-    if (isCycle(adj)) {
+    if (loopDetection(adj)) {
         cout << "Loop detected" << endl;
-    } else {
+    }
+    else {
         cout << "No loop detected" << endl;
     }
 }
